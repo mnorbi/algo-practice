eac3ef5a00576363d8dd29b93eceeef1a9983705
Norbert Madarász <mnorbi@mnorbit510.(none)>
2015-08-08 19:12:36 +0200

    practice problems

--- backtracking/BinPack.java
+++ backtracking/BinPack.java
@@ -5,9 +5,9 @@ class BinPack{
         binPack(new int[]{1, 1, 1, 1, 1}, 2);
     }
     static void binPack(int[] arr, int limit){
-        State st = new State(-1, new int[arr.length], 0);
+        State st = new State(0, new int[arr.length], 0);
         State solution = binPack(arr, limit, st, null);
-        System.out.println(solution.lastBucket + 1);
+        System.out.println(solution.bucketSize);
     }
     static State binPack(int[] arr, int limit, State st, State minSofar){
         if (st.isSolution(arr)){
@@ -24,39 +24,46 @@ class BinPack{
 
 class State{
     int[] alloc;
-    int lastItem;
-    int lastBucket;
+    int doneSize;
+    int bucketSize;
 
-    State(int lastItem, int[] alloc, int lastBucket){
-        this.lastItem = lastItem;
-        this.alloc = Arrays.copyOfRange(alloc, 0, alloc.length);
-        this.lastBucket = lastBucket;
+    State(int doneSize, int[] alloc, int bucketSize){
+        this.doneSize = doneSize;
+        this.alloc = Arrays.copyOfRange(alloc, 0, bucketSize+1);
+        this.bucketSize = bucketSize;
     }
     static State min(State st1, State st2){
         if (st1 == null) return st2;
         if (st2 == null) return st1;
-        if (st1.lastBucket > st2.lastBucket) {
+        if (st1.bucketSize > st2.bucketSize) {
             return st2;
         }
         return st1;
     }
     boolean isSolution(int[]arr){
-        return lastItem >= arr.length-1;
+        return doneSize == arr.length;
     }
     List<State> nextStates(int[]arr, int limit, State minSofar){
         if (min(this, minSofar) != this){
             return Collections.emptyList();
         }
         List<State> ret = new ArrayList<>();
-        int nextItem = lastItem+1;
-        for(int i = 0; i <= (minSofar == null ? nextItem : Math.min(nextItem, minSofar.lastBucket)); ++i){
-            alloc[i] += arr[nextItem];
+        for(int i = 0; i < untilBucket(doneSize+1, minSofar); ++i){
+            alloc[i] += arr[doneSize];
             if (alloc[i] <= limit){
-                ret.add(new State(nextItem, alloc, Math.max(i, this.lastBucket)));
+                ret.add(new State(doneSize+1, alloc, Math.max(i+1, this.bucketSize)));
             }
-            alloc[i] -= arr[nextItem];
+            alloc[i] -= arr[doneSize];
             if (alloc[i] == 0) break;
         }
         return ret;
+    }
+
+    private int untilBucket(int doneSize, State minSofar) {
+        if (minSofar == null){
+            return doneSize;
+        } else {
+            return Math.min(minSofar.bucketSize, doneSize);
+        }
     }
 }
