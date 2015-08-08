95d35ea0ecdf5a7e2bf252f94209a70f54e16ea8
Norbert Madarász <mnorbi@mnorbit510.(none)>
2015-08-08 18:36:11 +0200

    practice problems

--- backtracking/BinPack.java
+++ backtracking/BinPack.java
@@ -1,56 +1,59 @@
 import java.util.*;
 class BinPack{
-  public static void main(String[]args){
-    binPack(new int[]{10,23,14,6}, 30);
-  }
-  static void binPack(int[] arr, int limit){
-    State st = new State(-1, new int[arr.length], 0);
-    binPack(arr, limit, st);
-  }
-  static State binPack(int[] arr, int limit, State st){
-    if (st.isSolution(arr)){
-      return st;
+    public static void main(String[]args) {
+        binPack(new int[]{10, 23, 14, 6}, 30);
+        binPack(new int[]{1, 1, 1, 1, 1}, 2);
     }
-    State min = null;
-    for(State nextState : st.nextStates(arr, limit)){
-      State nextSolution = binPack(arr, limit, nextState);
-      min = State.min(min, nextSolution);
+    static void binPack(int[] arr, int limit){
+        State st = new State(-1, new int[arr.length], 0);
+        State solution = binPack(arr, limit, st);
+        System.out.println(solution.lastBucket + 1);
     }
-    return min;
-  }
+    static State binPack(int[] arr, int limit, State st){
+        if (st.isSolution(arr)){
+            return st;
+        }
+        State betterSolution = null;
+        for(State nextState : st.nextStates(arr, limit)){
+            State nextSolution = binPack(arr, limit, nextState);
+            betterSolution = State.betterSolution(betterSolution, nextSolution);
+        }
+        return betterSolution;
+    }
 }
 
 class State{
-  int[] alloc;
-  int lastItem;
-  int lastBucket;
+    int[] alloc;
+    int lastItem;
+    int lastBucket;
 
-  State(int lastItem, int[] alloc, int lastBucket){
-    this.lastItem = lastItem;
-    this.alloc = Arrays.copyOfRange(alloc, 0, alloc.length);
-    this.lastBucket = lastBucket;
-  }
-  static State min(State st1, State st2){
-    if (st1 == null) return st2;
-    if (st2 == null) return st1;
-    if (st1.lastBucket > st2.lastBucket) {
-      return st2;
+    State(int lastItem, int[] alloc, int lastBucket){
+        this.lastItem = lastItem;
+        this.alloc = Arrays.copyOfRange(alloc, 0, alloc.length);
+        this.lastBucket = lastBucket;
     }
-    return st1;
-  }
-  boolean isSolution(int[]arr){
-    return lastItem >= arr.length-1;
-  }
-  List<State> nextStates(int[]arr, int limit){
-    List<State> ret = new ArrayList<>();
-    int nextItem = lastItem+1;
-    for(int i = 0; i <= lastBucket+1; ++i){
-        alloc[i] += arr[nextItem];
-        if (alloc[i] <= limit){
-          ret.add(new State(nextItem, alloc, i));
+    static State betterSolution(State st1, State st2){
+        if (st1 == null) return st2;
+        if (st2 == null) return st1;
+        if (st1.lastBucket > st2.lastBucket) {
+            return st2;
         }
-        alloc[i] -= arr[nextItem];
+        return st1;
     }
-    return ret;
-  }
+    boolean isSolution(int[]arr){
+        return lastItem >= arr.length-1;
+    }
+    List<State> nextStates(int[]arr, int limit){
+        List<State> ret = new ArrayList<>();
+        int nextItem = lastItem+1;
+        for(int i = 0; i <= nextItem; ++i){
+            alloc[i] += arr[nextItem];
+            if (alloc[i] <= limit){
+                ret.add(new State(nextItem, alloc, Math.max(i, this.lastBucket)));
+            }
+            alloc[i] -= arr[nextItem];
+        }
+        return ret;
+    }
 }
+
