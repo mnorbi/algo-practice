3881f34aa55cc376d0cf5b6492343e5eaf64917d
Norbert Madarász <mnorbi@mnorbit510.(none)>
2015-11-28 23:45:39 +0100

    practice

--- prismo/ShortestRangeCoveringAllLists.java
+++ prismo/ShortestRangeCoveringAllLists.java
@@ -22,71 +22,71 @@ class ShortestRangeCoveringAllLists{
         System.out.println(Arrays.toString(sol2));
     }
     static int[] solve(int[][] mat){
-        Comparator<Item> comp = (i1, i2) -> Integer.compare(mat[i1.row][i1.col], mat[i2.row][i2.col]);
-        Item rangeStart = findRangeStart(mat, comp);
+        Comparator<Cell> comp = (i1, i2) -> Integer.compare(mat[i1.row][i1.col], mat[i2.row][i2.col]);
+        Cell rangeStart = findRangeStart(mat, comp);
         return createRange(mat, comp, rangeStart);
     }
 
-    private static Item findRangeStart(int[][] mat, Comparator<Item> comp) {
+    private static Cell findRangeStart(int[][] mat, Comparator<Cell> comp) {
         int ROWS = mat.length;
         int COLS = mat[0].length;
-        PriorityQueue<Item> minPq = new PriorityQueue<>(comp);
-        minPq.add(new Item(0, 0));
-        Item max = minPq.peek();
+        PriorityQueue<Cell> minPq = new PriorityQueue<>(comp);
+        minPq.add(new Cell(0, 0));
+        Cell max = minPq.peek();
         for(int i = 1; i < ROWS; ++i){
-            Item item = new Item(i, 0);
-            minPq.add(item);
-            if (comp.compare(max, item) < 0){
-                max = item;
+            Cell cell = new Cell(i, 0);
+            minPq.add(cell);
+            if (comp.compare(max, cell) < 0){
+                max = cell;
             }
         }
-        Item ret = null;
+        Cell ret = null;
         int widthOpt = Integer.MAX_VALUE;
         while(true){
-            Item min = minPq.remove();
+            Cell min = minPq.remove();
             int width = mat[max.row][max.col]-mat[min.row][min.col];
             if (widthOpt > width){
                 widthOpt = width;
                 ret = min;
             }
             if (min.col == COLS-1) break;
-            Item item = new Item(min.row, min.col +1);
-            minPq.add(item);
-            if (comp.compare(max, item) < 0){
-                max = item;
+            Cell cell = new Cell(min.row, min.col +1);
+            minPq.add(cell);
+            if (comp.compare(max, cell) < 0){
+                max = cell;
             }
         }
         return ret;
     }
 
-    private static int[] createRange(int[][] mat, Comparator<Item> comp, Item rangeStart) {
+    private static int[] createRange(int[][] mat, Comparator<Cell> comp, Cell rangeStart) {
         int ROWS = mat.length;
         int[] ret = new int[ROWS];
-        PriorityQueue<Item> minPq = new PriorityQueue<>(comp);
+        PriorityQueue<Cell> minPq = new PriorityQueue<>(comp);
         for(int i = 0; i < ROWS; ++i){
-            Item item = new Item(i, 0);
-            minPq.add(item);
+            Cell cell = new Cell(i, 0);
+            minPq.add(cell);
         }
 
         for(int i = 0;;){
-            Item min = minPq.peek();
+            Cell min = minPq.peek();
             if (min.row == rangeStart.row && min.col == rangeStart.col){
                 while(!minPq.isEmpty() ){
-                    Item item = minPq.remove();
-                    ret[i++] = mat[item.row][item.col];
+                    Cell cell = minPq.remove();
+                    ret[i++] = mat[cell.row][cell.col];
                 }
                 return ret;
             }
             minPq.remove();
-            Item item = new Item(min.row, min.col+1);
-            minPq.add(item);
+            Cell cell = new Cell(min.row, min.col+1);
+            minPq.add(cell);
         }
     }
 
 }
-class Item{
+class Cell {
     int row, col;
-    Item(int row, int col){
+    Cell(int row, int col){
         this.row = row;
         this.col = col;
     }
