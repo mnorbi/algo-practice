7ec20ab6e5d885da2f9fe4951c63d7d3ada6bfb4
Norbert Madarász <mnorbi@mnorbit510.(none)>
2016-04-14 23:08:53 +0200

    uva 1225 clarification comments

--- cp3/05-mathematics/01-ad-hoc/1225-digit-counting/1225.java
+++ cp3/05-mathematics/01-ad-hoc/1225-digit-counting/1225.java
@@ -8,7 +8,7 @@ class Main{
       int[] digits = new int[10];
       for(int digit = 0; digit < 10; ++digit){
         digits[digit] = 0;
-        for(int j = 1; N/j > 0; j*=10){
+        for(int j = 1; N/j > 0; j*=10){//cut out mid at different positions from N and replace them with digit
           int pfx = N/j/10;
           int sfx = N%j;
           int mid = (N-pfx*j*10)/j;
@@ -16,17 +16,17 @@ class Main{
 	  if (pfx == 0 && (digit > mid || digit == 0)) break;
 
           if (digit < mid){
-            digits[digit] += pfx*j+(j-1)+1;//concatenate prefix with sfx rounded up to 99...9, +1 from all zero values
+            digits[digit] += pfx*j+(j-1)+1;//concatenate prefix with sfx rounded up to 99...9, +1 from 0000[digit]0000 case
           } else if (digit > mid){
             if (pfx > 0){
               digits[digit] += (pfx-1)*j;//concatenate decreased prefix
             }
-            digits[digit] += (j-1)+1;//with 99...9 from suffix, +1 from all zero values
+            digits[digit] += (j-1)+1;//with 99...9 from suffix, +1 from 0000[digit]0000 case
           } else {
-            digits[digit] += pfx*j+sfx+1;//concatenate prefix and suffix, +1 from all zero values
+            digits[digit] += pfx*j+sfx+1;//concatenate prefix and suffix, +1 from 0000[digit]0000 case
           }
 
-          int zeroCorrection = (digit == 0) ? -j : 0;//if digit==0 and prefix not yet active, the placed digit at j+1th does not yield valid number, because of overcounting previously we need to decrease the count with all the numbers from [0,99..9] which is actually j
+          int zeroCorrection = (digit == 0) ? -j : 0;//if digit==0 and counting 000[digitAtJPlusOne]abcd, [digitAtJPlusOne]abcd is actually 0abcd which is invalid, we have to decrease the sum with these invalid ones, if sfx length is 4 (so j=10**4) than there are [0000 ... 9999] of them, so j needs to be subtracted
 	  digits[digit] += zeroCorrection;
         }
         if (digit > 0) System.out.print(" ");
