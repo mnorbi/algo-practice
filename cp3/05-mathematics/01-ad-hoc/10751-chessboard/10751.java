ea5621474e8eb941b9e74ff15947e761d455ff44
Norbert Madarász <mnorbi@mnorbit510.(none)>
2016-04-19 22:26:43 +0200

    uva 10751 reminder comment

--- cp3/05-mathematics/01-ad-hoc/10751-chessboard/10751.java
+++ cp3/05-mathematics/01-ad-hoc/10751-chessboard/10751.java
@@ -1,4 +1,28 @@
 import java.util.*;
+
+/**
+
+XXXXXXX      XXXXXXXXXXXX       XXXXXXXXXXX
+XX    X    XXX       XXX      XXX        XX
+XX    X  XXX       XXX    XXXXX          XX
+XX    XXXX      XXXX    XXX       XXX    XX
+XX    XX     XXXX    XXXX      XXX  X    XX
+XX         XXX     XXX       XXX    X    XX
+XX       XX      XXX       XX       X    XX
+XX    XXXX     XXX       XX         X    XX
+XX    XX     XX      XXXX       XXXXX    XX
+XX    XX  XXX       XX         XX        XX
+XX    XXXXX      XXX         XXX         XX
+XX    XXX      XXX         XXX           XX
+XX            XX         XXX        XX   XX
+XX        XXXXX      XXXXX        XXX    XX
+XX      XXX       XXXX         XXXX X    XX
+XX    XXX      XXXX         XXXX    X    XX
+XX  XXX     XXXX          XXX       X    XX
+XXXX       XX          XXXX         X    XX
+XXX       XXXXXXXXXXXXXXX           XXXXXXX
+
+**/
 class Main{
   public static void main(String[]args){
     Scanner sc = new Scanner(System.in);
