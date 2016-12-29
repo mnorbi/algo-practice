efce4a678d877978feac0bac3c44a9d5e9913dc8
Norbert Madarász <mnorbi@mnorbit510.(none)>
2016-12-29 09:09:10 +0100

    bugfix

--- aziz/FindTheFirstMissingPositiveEntry_Question22dot2.java
+++ aziz/FindTheFirstMissingPositiveEntry_Question22dot2.java
@@ -7,6 +7,8 @@ public class FindTheFirstMissingPositiveEntry_Question22dot2{
         System.out.println(firstMissingPositiveEntry(new int[]{5, 4, 3, 2, 1, -1, 0, 0, -1}));
         System.out.println(firstMissingPositiveEntry(new int[]{-1, 1, 4, -1, 3, 2, 1, 1000}));
         System.out.println(firstMissingPositiveEntry(new int[]{3, 5, 4, -1, 5, 1, -1}));
+        System.out.println(firstMissingPositiveEntry(new int[]{1,2,3,4,5,1,1,1,1,1,1,1}));
+        System.out.println(firstMissingPositiveEntry(new int[]{1,1,1,1,1,1,1,1,2,3,4,5}));
     }
 
     static int firstMissingPositiveEntry(int[] arr) {
@@ -15,16 +17,15 @@ public class FindTheFirstMissingPositiveEntry_Question22dot2{
             int val = arr[p-1];
             if (val < p || val > n) {swap(arr, p-1, --n);}
             else if (val == p) {++p;}
-            else if (!swap(arr, p-1, val-1)){ return p; }
+            else if (val == arr[val-1]) {swap(arr,p-1,--n);}
+            else { swap(arr, p-1, val-1); }
         }
         return p;
     }
 
-    static boolean swap(int[] arr, int i, int j) {
-        if (arr[i] == arr[j]) return false;
+    static void swap(int[] arr, int i, int j) {
         int tmp = arr[i];
         arr[i] = arr[j];
         arr[j] = tmp;
-        return true;
     }
 }
