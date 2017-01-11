8aee7199c617f5d5c25ddc63c56bf03153b346a2
Norbert Madarász <mnorbi@mnorbit510.(none)>
2017-01-11 23:16:57 +0100

    comments

--- other/ArrangeToLargestNumber.java
+++ other/ArrangeToLargestNumber.java
@@ -1,4 +1,9 @@
 import java.util.*;
+/*
+Write a function that given a list of non negative integers,
+arranges them such that they form the largest possible number.
+For example, given [50, 2, 1, 9], the largest formed number is 95021
+*/
 class ArrangeToLargestNumber{
   public static void main(String[]args){
     Integer[] arr = new Integer[]{5, 50, 56};
--- other/CombinationsToHundred.java
+++ other/CombinationsToHundred.java
@@ -1,4 +1,10 @@
 import java.util.*;
+/*
+Write a program that outputs all possibilities to put
++ or - or nothing between the numbers 1, 2, ..., 9
+(in this order) such that the result is always 100.
+For example: 1 + 2 + 34 – 5 + 67 – 8 + 9 = 100.
+*/
 class CombinationsToHundred{
   public static void main(String[]args){
     solve(new ArrayList<String>(Arrays.asList("+", "1")),1,2);
