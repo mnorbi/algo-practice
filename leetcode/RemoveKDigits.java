fef80c53baf00887240ea51b3005951df27d984e
Norbert Madarász <mnorbi@mnorbit510.(none)>
2017-01-14 20:05:13 +0100

    -

--- leetcode/RemoveKDigits.java
+++ leetcode/RemoveKDigits.java
@@ -4,27 +4,25 @@ import java.util.LinkedList;
 class RemoveKDigits{
   public static void main(String[] args) {
     RemoveKDigits solver = new RemoveKDigits();
-    System.out.println(
-      solver.removeKDigits(
-        "1432219", 3
-      )
-    );
+    System.out.println(solver.removeKDigits("10200300", 2));
+    System.out.println(solver.removeKDigits("1432219", 3));
   }
 
   String removeKDigits(String s, int k) {
     if (s == null || s.length() <= k) return "0";
     Deque<Character> stack = new LinkedList<>();
 
+    /*stack invariants:
+       - is always non decreasing
+       - must never contain the 0 element
+    */
     int a = 0;
     while(a < s.length() && k > 0){
       char c = s.charAt(a);
-      if (stack.isEmpty()){
+      if (stack.isEmpty() || stack.peekLast() <= c){
         if (c != '0'){
           stack.addLast(c);
         }
-        ++a;
-      } else if (stack.peekLast() <= c){
-        stack.addLast(c);
         ++a;
       } else {
         stack.removeLast();
@@ -32,19 +30,11 @@ class RemoveKDigits{
       }
     }
 
-    if (stack.isEmpty()) {
-      while (a < s.length() && s.charAt(a) == '0') {
-        ++a;
-      }
-    }
+    if (stack.isEmpty()) { while (a < s.length() && s.charAt(a) == '0') { ++a; } }
 
     StringBuilder sb = new StringBuilder();
-    while(!stack.isEmpty()){
-      sb.append(stack.removeFirst());
-    }
-    if (a < s.length()) {
-      sb.append(s.substring(a));
-    }
+    while(!stack.isEmpty()){ sb.append(stack.removeFirst()); }
+    if (a < s.length()) { sb.append(s.substring(a)); }
 
     return sb.length() > 0 ? sb.toString() : "0";
   }
