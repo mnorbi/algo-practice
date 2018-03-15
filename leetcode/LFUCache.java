1df1e5995b415e31a1e2bdafb9cf47dd500271bb
Norbert Madarász <norbert.madarasz@gmail.com>
2018-03-15 06:26:38 +0100

    https://leetcode.com/problems/lfu-cache/

--- leetcode/LFUCache.java
+++ leetcode/LFUCache.java
@@ -1,141 +1,71 @@
-import java.util.*;
-
-public class LFUCache {
-    private final int capacity;
-    private Map<Integer,Integer> map = new HashMap<>();
-    private FreqTable freqTable = new FreqTableImpl();
+class LFUCache {
+    Map<Integer, Integer> map = new HashMap<>();
+    Map<Integer, Integer> freq = new HashMap<>();
+    Map<Integer, LinkedHashSet<Integer>> lru = new LinkedHashMap<>();
+    int min = 0;
+    int free;
+    int capacity;
+    
     public LFUCache(int capacity) {
         this.capacity = capacity;
+        this.free = capacity;
     }
-
+    
     public int get(int key) {
         if (map.containsKey(key)){
-            freqTable.increaseFreq(key);
+            updateFreq(key);
             return map.get(key);
         }
         return -1;
     }
-
-    public void set(int key, int value) {
-        if (capacity == 0) return;
-        if (map.containsKey(key)){
-            freqTable.increaseFreq(key);
-            map.put(key, value);
-        } else {
-            if (capacity <= map.size()){
-                map.remove(
-                        freqTable.removeLfu());
-            }
-            map.put(key, value);
-            freqTable.add(key);
-        }
-    }
-
-    public static void main(String[] args) {
-        LFUCache lfuCache = new LFUCache(2);
-        lfuCache.set(1, 1);
-        lfuCache.set(2, 2);
-        lfuCache.get(1);
-        lfuCache.set(3, 3);
-        lfuCache.get(2);
-        lfuCache.get(3);
-        lfuCache.set(4, 4);
-        lfuCache.get(1);
-        lfuCache.get(3);
-        lfuCache.get(4);
-    }
-}
-interface FreqTable{
-    void increaseFreq(int key);
-    int removeLfu();
-    void add(int key);
-}
-class FreqTableImpl implements FreqTable{
-    Map<Integer, MyNode<Bucket>> bucketInd = new HashMap<>();
-    MyNode<Bucket> head;
-    FreqTableImpl(){
-        head = new MyNode<>(new Bucket(0));
-        head.l = head;
-        head.r = head;
-    }
-    public void increaseFreq(int key){
-        MyNode<Bucket> node = bucketInd.get(key);
-        if (node == null) return;
-        int freq = (node.val.freq)+1;
-        node.val.keys.remove(key);
-        if (node.val.keys.size() == 0){
-            bucketInd.remove(node);
-            MyNode<Bucket> l = node.l;
-            MyNode<Bucket> r = node.r;
-            l.r = r;
-            r.l = l;
-            node = l;
-        }
-        if (node.r.val.freq != freq){
-            MyNode<Bucket> next = new MyNode<>(new Bucket(freq));
-            next.val.keys.add(key);
-            MyNode<Bucket> tmp = node.r;
-            node.r = next;
-            next.l = node;
-            next.r = tmp;
-            tmp.l = next;
-            bucketInd.put(key, next);
-        } else {
-            node.r.val.keys.add(key);
-            bucketInd.put(key,node.r);
-        }
-    }
-    public void add(int key){
-        if (head.r.val.freq != 1){
-            MyNode<Bucket> tmp = head.r;
-            MyNode<Bucket> next = new MyNode<>(new Bucket(1));
-            next.val.keys.add(key);
-            head.r = next;
-            next.l = head;
-            next.r = tmp;
-            tmp.l = next;
-            bucketInd.put(key, next);
-        } else{
-            head.r.val.keys.add(key);
-            bucketInd.put(key, head.r);
-        }
-    }
-    public int removeLfu(){
-        if (head.r != head){
-            MyNode<Bucket> node = head.r;
-            if (!node.val.keys.isEmpty()){
-                int key = node.val.keys.iterator().next();
-                bucketInd.remove(key);
-                node.val.keys.remove(key);
-                if (node.val.keys.size() == 0) {
-                    MyNode<Bucket> l = node.l;
-                    MyNode<Bucket> r = node.r;
-                    l.r = r;
-                    r.l = l;
+    
+    private void updateFreq(int key){
+        int f = freq.getOrDefault(key, 0);
+        if (lru.containsKey(f)){
+            lru.get(f).remove(key);
+            if (lru.get(f).isEmpty()){
+                lru.remove(f);
+                if (min == f){
+                    ++min;
                 }
-                return key;
             }
         }
-        return -1;
+        lru.computeIfAbsent(f+1, LinkedHashSet::new).add(key);
+        freq.merge(key, 1, Integer::sum);
+    }
+    
+    public void put(int key, int value) {
+        if (capacity <= 0) return;
+
+        if (!map.containsKey(key)){
+            if (free == 0){
+                evict();
+            } else {
+                --free;
+            }
+            min = 1;
+        }
+        map.put(key, value);
+        updateFreq(key);
+    }
+    
+    private void evict(){
+        if (lru.containsKey(min) && lru.get(min).size() > 0){
+            Iterator<Integer> it = lru.get(min).iterator();
+            Integer key = it.next();
+            it.remove();
+            if (lru.get(min).isEmpty()){
+                lru.remove(min);
+            }
+            freq.remove(key);
+            map.remove(key);
+        }
     }
 }
-class MyNode<T>{
-    T val;
-    MyNode<T> l, r;
-    MyNode(T val){
-        this.val = val;
-    }
-}
-class Bucket{
-    int freq;
-    Set<Integer> keys = new LinkedHashSet<>();
-    Bucket(int freq){
-        this.freq = freq;
-    }
-}
+
 /**
  * Your LFUCache object will be instantiated and called as such:
  * LFUCache obj = new LFUCache(capacity);
  * int param_1 = obj.get(key);
- * obj.set(key,value);
+ * obj.put(key,value);
  */
