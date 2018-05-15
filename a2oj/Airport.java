db047896820a7afcb7449e077b0c8a331e8710e1
Norbert Madarász <norbert.madarasz@gmail.com>
2018-05-15 07:23:07 +0200

    codeforces practice

--- a2oj/Airport.java
+++ a2oj/Airport.java
@@ -2,29 +2,42 @@ import java.util.*;
 import java.io.*;
 import static java.lang.Character.isWhitespace;
 
-public class CodeForcesTemplate{
+public class Airport{
   public static void main(String[]args)throws Exception{
     try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))){
-      String line = br.readLine();
+      int[] arr = ints(br.readLine(),2);
+      int n = arr[0], m = arr[1];
+      arr = ints(br.readLine(), m);
+
+      PriorityQueue<Integer> pq = new PriorityQueue<>((v1,v2) -> -Integer.compare(v1,v2));
+      for(int a = 0; a < m; ++a){
+        pq.add(arr[a]);
+      }
+      int max = 0;
+      for(int a = n; a > 0; --a){
+        int v = pq.remove();
+        max += v;
+        --v;
+        pq.add(v);
+      }
+
+      Arrays.sort(arr);
+      int min = 0;
+      for(int a = n, b = 0; a > 0;--a){
+        if (arr[b] > 0){
+          min += arr[b];
+          --arr[b];
+        } else {
+          ++b;
+          ++a;
+        }
+      }
+
+      System.out.println(max + " " + min);
     }
   }
   private static int[] ints(String s, int n){
     int[] arr = new int[n];
-    char c, pc = ' ';
-    int len = s.length();
-    for(int a = 0, hi = 0;hi < len;++hi){
-      c = s.charAt(hi);
-      if (!isWhitespace(c)){
-        arr[a] *= 10; arr[a] += (c-'0');
-      } else {
-        a += !isWhitespace(pc) ? 1 : 0;
-      }
-      pc = c;
-    }
-    return arr;
-  }
-  private static long[] longs(String s, int n){
-    long[] arr = new long[n];
     char c, pc = ' ';
     int len = s.length();
     for(int a = 0, hi = 0;hi < len;++hi){
--- a2oj/EvenOdds.java
+++ a2oj/EvenOdds.java
@@ -2,10 +2,20 @@ import java.util.*;
 import java.io.*;
 import static java.lang.Character.isWhitespace;
 
-public class CodeForcesTemplate{
+public class EvenOdds{
   public static void main(String[]args)throws Exception{
     try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))){
-      String line = br.readLine();
+      long[] arr = longs(br.readLine(),2);
+      long n = arr[0], k = arr[1];
+      long oddCnt = (n+1)/2;
+      long ans = 0;
+      if (k <= oddCnt){
+	ans = 1 + (k-1<<1);//#bug #precedence << has lower precedence than +
+      } else {
+	k -= oddCnt;
+        ans = k<<1;
+      }
+      System.out.println(ans);
     }
   }
   private static int[] ints(String s, int n){
