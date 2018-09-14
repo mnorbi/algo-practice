c3eb0d438d48800c8fd6197e85735c587f03dd07
Norbert Madarász <norbert.madarasz@gmail.com>
2018-09-14 21:23:29 +0200

    https://codejam.withgoogle.com/codejam/contest/4214486/dashboard

---
+++ google-code-jam/2014-round-b/A.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.io.*;
+public class A {
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            int t = Integer.valueOf(br.readLine());
+            for (int i = 1; i <= t; ++i) {
+                int MOD = 1_000_000_007;
+                int[] arr = toIntArr(br.readLine());
+                int M = arr[0];
+                int N = arr[1];
+                long[] dp = new long[M+1];
+                dp[1] = 1;
+                for(int n = 2; n <= N; ++n){
+                    long pre = 1;
+                    for(int m = 2; m <= Math.min(M, n); ++m){
+                        long tmp = dp[m];
+                        if (n <= m){
+                            dp[m] = 0;
+                        }
+                        dp[m] += pre;
+                        dp[m] *= m;
+                        dp[m] %= MOD;
+                        pre = tmp;
+                    }
+                }
+                System.out.println("Case #" + i + ": " + dp[M]);
+            }
+        }
+    }
+
+    static int[] toIntArr(String s) {
+        return Arrays.stream(s.split("\\s+")).mapToInt(Integer::valueOf).toArray();
+    }
+}
